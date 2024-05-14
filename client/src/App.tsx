fd8e30a0c1e8e29721ab7de6ab372dee98ace198
igachev <ivophoenix1135@gmail.com>
2024-05-14 22:57:41 +0300

    created container component

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Container from './components/Container/Container'
+
+
+function App() {
+  
+
+  return (
+    <div>
+      <h1>snake game</h1>
+     <Container></Container>
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/components/Container/Container.tsx
@@ -0,0 +1,12 @@
+
+import "./Container.css"
+
+export default function Container({children}:any) {
+
+    return (
+        <div className="container">
+            
+            {children}
+        </div>
+    )
+}
