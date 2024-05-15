d007365292b82aa5d494a0262cc71efe6656a051
igachev <ivophoenix1135@gmail.com>
2024-05-15 12:13:28 +0300

    added functionality to move the snake up

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,55 @@
 
+import { useEffect, useState } from 'react'
 import './App.css'
 import AllSquares from './components/AllSquares/AllSquares'
 import Container from './components/Container/Container'
 import SnakeHead from './components/SnakeHead/SnakeHead'
 
+interface Position {
+  bottom: number;
+  left: number;
+}
 
 function App() {
   
+  let [position,setPosition] = useState<Position>({bottom:400,left:550})
+  let [timer,setTimer] = useState<number>()
+  
+  // useEffect() for moving the snake up
+  useEffect(() => {
+
+    function moveUp(e: KeyboardEvent) {
+      e.preventDefault()
+      if(e.key === 'ArrowUp') {
+        
+       let repeatedMovement = () => {
+        let snakeHead = document.querySelector('.snake-head') as HTMLDivElement
+        let bottom = getComputedStyle(snakeHead).getPropertyValue('bottom')
+        let updateBottom = parseInt(bottom) + 50
+        setPosition((oldPosition) => ({...oldPosition, bottom: updateBottom}))
+        }
+
+        let timerAction = setInterval(repeatedMovement,500)
+        setTimer(timerAction)
+      }
+      else {
+        clearInterval(timer)
+      }
+      
+    }
+
+    document.addEventListener("keydown",moveUp)
+    return () => {
+      document.removeEventListener("keydown",moveUp)
+     // clearInterval(timer)
+    }
+  },[position])
+
+  // useEffect() to update snakeHead DOM element when the position changes
+  useEffect(() => {
+    let snakeHead = document.querySelector('.snake-head') as HTMLDivElement;
+    snakeHead.style.bottom = `${position.bottom}px`;
+  }, [position]);
 
   return (
     <div>
