4a7d557bb79aad1a3270a61831431b31434a0e57
igachev <ivophoenix1135@gmail.com>
2024-05-15 22:15:48 +0300

    changed the approach how the snake moves.
    Used useRef() hook instead of useState() in order to preserve the value of timer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 import AllSquares from './components/AllSquares/AllSquares'
 import Container from './components/Container/Container'
@@ -13,45 +13,41 @@ interface Position {
 function App() {
   
   let [position,setPosition] = useState<Position>({bottom:400,left:550})
-  let [timer,setTimer] = useState<number>()
-  let [isMovingUp,setIsMovingUp] = useState<boolean>(false)
+ // let [timer,setTimer] = useState<number>(0)
+ const timerRef = useRef<number | undefined>();
   
   // useEffect() for moving the snake up
   useEffect(() => {
 
     function moveUp(e: KeyboardEvent) {
-      e.preventDefault()
-
-      if(e.key === 'ArrowUp') {
-       
-       let repeatedMovement = () => {
-        let snakeHead = document.querySelector('.snake-head') as HTMLDivElement
-        let bottom = getComputedStyle(snakeHead).getPropertyValue('bottom')
-        let updateBottom = parseInt(bottom) + 50
-        setPosition((oldPosition) => ({...oldPosition, bottom: updateBottom}))
-        }
-
-        if(isMovingUp) {
-          return;
-        } 
-        let timerAction = setInterval(repeatedMovement,500)
-        setTimer(timerAction)
-        setIsMovingUp(true)
-    }
-      else {
-        clearInterval(timer)
+      e.preventDefault();
+  
+      if (e.key === 'ArrowUp') {
+        clearInterval(timerRef.current);
+  
+        const movement = () => {
+          const snakeHead = document.querySelector('.snake-head') as HTMLDivElement;
+          const bottom = getComputedStyle(snakeHead).getPropertyValue('bottom');
+          const updateBottom = parseInt(bottom) + 50;
+          console.log('rep');
+          setPosition((oldPosition) => ({ ...oldPosition, bottom: updateBottom }));
+        };
+  
+        timerRef.current = setInterval(movement, 500);
+      } else {
+        console.log('clear');
+        clearInterval(timerRef.current);
       }
-    
-    }
-      
+    };
+
       document.addEventListener("keyup",moveUp)
-    
     return () => {
       document.removeEventListener("keyup",moveUp)
-     // clearInterval(timer)
-     
     }
   },[position])
+
+   // useEffect() for moving the snake down
+  
 
   // useEffect() to update snakeHead DOM element when the position changes
   useEffect(() => {
