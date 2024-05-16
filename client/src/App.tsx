9a8f65f64638041f8a11e42f52c6790dd66f81e3
igachev <ivophoenix1135@gmail.com>
2024-05-16 13:27:59 +0300

    called useMovement hook in App.tsx
    I added several useEffect() hooks to listen for following keydown events:
    1)arrowup 2)arrowdown 3)arrowleft 4)arrowright

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,8 +4,10 @@ import './App.css'
 import AllSquares from './components/AllSquares/AllSquares'
 import Container from './components/Container/Container'
 import SnakeHead from './components/SnakeHead/SnakeHead'
+import { useMovement } from './hooks/useMovement'
 
-interface Position {
+
+export interface Position {
   bottom: number;
   left: number;
 }
@@ -13,46 +15,51 @@ interface Position {
 function App() {
   
   let [position,setPosition] = useState<Position>({bottom:400,left:550})
- // let [timer,setTimer] = useState<number>(0)
- const timerRef = useRef<number | undefined>();
+  const timerRef = useRef<number | undefined>();
+  let moveUp = useMovement('ArrowUp','up',setPosition,timerRef)
+  let moveDown = useMovement('ArrowDown','down',setPosition,timerRef)
+  let moveLeft = useMovement('ArrowLeft','left',setPosition,timerRef)
+  let moveRight = useMovement('ArrowRight','right',setPosition,timerRef)
+ 
   
   // useEffect() for moving the snake up
   useEffect(() => {
-
-    function moveUp(e: KeyboardEvent) {
-      e.preventDefault();
-  
-      if (e.key === 'ArrowUp') {
-        clearInterval(timerRef.current);
-  
-        const movement = () => {
-          const snakeHead = document.querySelector('.snake-head') as HTMLDivElement;
-          const bottom = getComputedStyle(snakeHead).getPropertyValue('bottom');
-          const updateBottom = parseInt(bottom) + 50;
-          console.log('rep');
-          setPosition((oldPosition) => ({ ...oldPosition, bottom: updateBottom }));
-        };
-  
-        timerRef.current = setInterval(movement, 500);
-      } else {
-        console.log('clear');
-        clearInterval(timerRef.current);
-      }
-    };
-
-      document.addEventListener("keyup",moveUp)
+    document.addEventListener("keydown", moveUp);
     return () => {
-      document.removeEventListener("keyup",moveUp)
+      document.removeEventListener("keydown", moveUp);
+      //clearInterval(timerRef.current);
+    };
+  }, [position]);
+
+   // useEffect() for moving the snake down
+  useEffect(() => {
+    document.addEventListener("keydown",moveDown)
+    return () => {
+      document.removeEventListener("keydown",moveDown)
     }
   },[position])
 
-   // useEffect() for moving the snake down
-  
+  // useEffect() for moving the snake left
+  useEffect(() => {
+    document.addEventListener("keydown",moveLeft)
+    return () => {
+      document.removeEventListener("keydown",moveLeft)
+    }
+  },[position])
+
+  // useEffect() for moving the snake right
+  useEffect(() => {
+    document.addEventListener("keydown",moveRight)
+    return () => {
+      document.removeEventListener("keydown",moveRight)
+    }
+  },[position])
 
   // useEffect() to update snakeHead DOM element when the position changes
   useEffect(() => {
     let snakeHead = document.querySelector('.snake-head') as HTMLDivElement;
     snakeHead.style.bottom = `${position.bottom}px`;
+    snakeHead.style.left = `${position.left}px`;
   }, [position]);
 
   return (
