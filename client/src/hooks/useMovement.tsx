3aaa3c591346e5ac60e663b07e7c71fb119bcd12
igachev <ivophoenix1135@gmail.com>
2024-05-18 14:01:13 +0300

    edited customHook useMovement
    in this hook I added parameter setSnakeLength which will be used there to update the body elements of the snake

--- client/src/hooks/useMovement.tsx
+++ client/src/hooks/useMovement.tsx
@@ -2,55 +2,65 @@
 import { Position } from "../App";
 
 export function useMovement(
-     keyValue: string,
-     direction: string,
-     setPosition: React.Dispatch<React.SetStateAction<Position>>,
-     timerRef:  React.MutableRefObject<number | undefined>
-    ) {
-    
-    function move(e: KeyboardEvent) {
-      e.preventDefault()
-      if (e.key === keyValue) {
-        // if another interval is already started it will be cleared
-        // to prevent unexpected behavior
-        clearInterval(timerRef.current);
-        
-        // updates the position values
-        function movement() {
-          let updateBottom: number = 0;
-          let updateLeft: number = 0;
+  keyValue: string,
+  direction: string,
+  setPosition: React.Dispatch<React.SetStateAction<Position>>,
+  timerRef: React.MutableRefObject<number | undefined>,
+  setSnakeLength: React.Dispatch<React.SetStateAction<Position[]>>
+) {
 
-          const snakeHead = document.querySelector('.snake-head') as HTMLDivElement;
+  function move(e: KeyboardEvent) {
+    e.preventDefault();
+    if (e.key === keyValue) {
+      clearInterval(timerRef.current);
 
-          const bottom = getComputedStyle(snakeHead).getPropertyValue('bottom');
-          let left = getComputedStyle(snakeHead).getPropertyValue('left');
+      function movement() {
+        let updateBottom: number = 0;
+        let updateLeft: number = 0;
 
-          if(direction === 'up') {
-             updateBottom = parseInt(bottom) + 50;
-             setPosition((oldPosition: Position) => ({ ...oldPosition, bottom: updateBottom }));
+        const snakeHead = document.querySelector('.snake-head') as HTMLDivElement;
+        if (!snakeHead) return;
+
+        let bottom = parseInt(getComputedStyle(snakeHead).getPropertyValue('bottom'));
+        let left = parseInt(getComputedStyle(snakeHead).getPropertyValue('left'));
+
+        if (direction === 'up') {
+          updateBottom = bottom + 50;
+          setPosition((oldPosition: Position) => ({ ...oldPosition, bottom: updateBottom }));
+          snakeHead.style.transform = 'rotate(0deg)';
+        } else if (direction === 'down') {
+          updateBottom = bottom - 50;
+          setPosition((oldPosition: Position) => ({ ...oldPosition, bottom: updateBottom }));
+          snakeHead.style.transform = 'rotate(180deg)';
+        } else if (direction === 'left') {
+          updateLeft = left - 50;
+          setPosition((oldPosition: Position) => ({ ...oldPosition, left: updateLeft }));
+          snakeHead.style.transform = 'rotate(270deg)';
+        } else if (direction === 'right') {
+          updateLeft = left + 50;
+          setPosition((oldPosition: Position) => ({ ...oldPosition, left: updateLeft }));
+          snakeHead.style.transform = 'rotate(90deg)';
+        }
+
+        // Update the snake parts
+        setSnakeLength((oldPositions) => {
+          const newPositions = [...oldPositions];
+          if (newPositions.length > 0) {
+            for (let i = newPositions.length - 1; i > 0; i--) {
+              newPositions[i] = { ...newPositions[i - 1] };
+            }
+            // first element of snake body will be equal to the position of the head
+            newPositions[0] = { bottom, left };
+            
           }
-          else if(direction === 'down') {
-            updateBottom = parseInt(bottom) - 50;
-            setPosition((oldPosition: Position) => ({ ...oldPosition, bottom: updateBottom }));
-          }
+          return newPositions;
+        });
 
-          else if(direction === 'left') {
-            updateLeft = parseInt(left) - 50;
-            setPosition((oldPosition: Position) => ({...oldPosition, left: updateLeft}))
-          }
-
-          else if(direction === 'right') {
-            updateLeft = parseInt(left) + 50;
-            setPosition((oldPosition: Position) => ({...oldPosition, left: updateLeft}))
-          }
-          
-        };
-        
-        // starts new interval so on each 0.5sec to move with 50px in the desired direction
-        timerRef.current = setInterval(movement, 500);
       }
-    };
-  
-   
-    return move;
+
+      timerRef.current = setInterval(movement, 500);
+    }
   }
+
+  return move;
+}
